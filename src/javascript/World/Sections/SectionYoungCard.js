069c6c306c35c389b0e1866b8c9e81fa1c2365c3
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2025-05-11 16:03:15 +0300

    Young Card Collision

--- src/javascript/World/Sections/SectionYoungCard.js
+++ src/javascript/World/Sections/SectionYoungCard.js
@@ -68,26 +68,22 @@ export default class SectionCapsule {
   
     // Bounding box hesapla
     baseModel.updateMatrixWorld(true);
-    const bbox = new THREE.Box3().setFromObject(baseModel);
+    const bbox = new THREE.Box3().setFromObject(this.container);
     var size = bbox.getSize(new THREE.Vector3());
   
     // Fizik gövdesi oluştur
-    const halfExtents = new CANNON.Vec3(size.x / 2, size.y / 2, size.z / 1);
+    const halfExtents = new CANNON.Vec3(size.x / 2, size.y /1.9, size.z / 1);
     const boxShape = new CANNON.Box(halfExtents);
   
     const body = new CANNON.Body({
       mass: 0,
-      position: new CANNON.Vec3(
-        this.position.x +2,
-        this.position.y,
-        this.position.z-2.8
-      ),
+      position: baseModel.position,
       material: this.physics.materials.items.floor,
     });
   
     // Dönüşü quaternion olarak ayarla
     const quat = new CANNON.Quaternion();
-    quat.setFromEuler(this.rotateX, this.rotateY, this.rotateZ, 'XYZ');
+    quat.setFromEuler(baseModel.rotation.x, baseModel.rotation.y, baseModel.rotation.z, 'XYZ');
     body.quaternion.copy(quat);
   
     body.addShape(boxShape);
