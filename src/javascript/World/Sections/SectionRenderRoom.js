6ee48299b8d0cb65af02721f2d46cf1b261b2241
akgunege1 <233301082@ogr.selcuk.edu.tr>
2025-05-11 19:52:31 +0300

    Added Z Indexes for Mini Divisions

--- src/javascript/World/Sections/SectionRenderRoom.js
+++ src/javascript/World/Sections/SectionRenderRoom.js
@@ -31,7 +31,7 @@ export default class SectionRenderRoom {
         loader.load('./models/SectionRenderRoom/base.glb', (gltf) => {
             
             this.model = gltf.scene;
-            this.model.position.set(-45, -10, -2);
+            this.model.position.set(-45, -10, 2);
             this.model.scale.set(2,2,2);
 
             this.scene.add(this.model);
