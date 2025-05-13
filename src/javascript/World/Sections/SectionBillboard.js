1bb56586123ccec23c433646cc4487b2ef594523
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2025-05-13 23:38:10 +0300

    Billboard Update

--- src/javascript/World/Sections/SectionBillboard.js
+++ src/javascript/World/Sections/SectionBillboard.js
@@ -126,8 +126,8 @@ export default class SectionBillboard {
     
     // Buton konumu - ses odası için uygun konum
     this.button.position = new THREE.Vector3(
-      billboardPosition.x - 2.5, // Modelin önünde
-      billboardPosition.y - 2, // Modelin önünde
+      billboardPosition.x , // Modelin önünde
+      billboardPosition.y - 4,
       billboardPosition + 0.25 // Zemin üzerinde, görünür olacak şekilde
     )
     
@@ -144,7 +144,7 @@ export default class SectionBillboard {
     // Canvas ile texture oluştur
     this.button.label.canvas = document.createElement('canvas')
     this.button.label.canvas.width = 512
-    this.button.label.canvas.height = 200
+    this.button.label.canvas.height = 360
     this.button.label.context = this.button.label.canvas.getContext('2d')
 
     // Texture oluştur
@@ -173,7 +173,7 @@ export default class SectionBillboard {
     // Alan tetikleyici oluştur
     this.button.triggerArea = this.areas.add({
         position: new THREE.Vector2(this.button.position.x, this.button.position.y),
-        halfExtents: new THREE.Vector2(1.4, 0.8),
+        halfExtents: new THREE.Vector2(2, 1.5),
         hasKey: true,
         testCar: true,
         active: true
